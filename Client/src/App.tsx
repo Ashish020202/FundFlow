8c96fc5b6e36136ebe8ce36e3e766a0ac75c8c6e
Ashish kumar <ashishgupta020202@gmail.com>
2025-02-02 13:20:26 +0530

    initalise project

---
+++ Client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-yellow-500'> 
+      ashish
+    </div>
+  )
+}
+
+export default App
---
+++ Server/src/app.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+
+dotenv.config();
+const app  = express();
+
+
+app.use(express.json());
+app.use(cors());
+
+const MONGO_URI = process.env.MONGO_URI ||'';
+console.log("uri",MONGO_URI);
+
+const PORT = process.env.PORT || 3000;
+
+
+
+
+mongoose.connect(MONGO_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('MongoDB connection error:', err));
+    app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+   
+
+export default app;
